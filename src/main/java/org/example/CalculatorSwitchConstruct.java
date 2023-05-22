16661ea1aa0f8cefdedb12f43a27528b9c15a35f
pranay <pranayanand61@gmail.com>
2023-05-22 17:32:44 +0530

    prompt the user to enter first number

--- src/main/java/org/example/CalculatorSwitchConstruct.java
+++ src/main/java/org/example/CalculatorSwitchConstruct.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 
 public class CalculatorSwitchConstruct {
     public static void main(String[] args) {
+        // prompt the scanner
         Scanner Scanner = new Scanner(Sustem.in);
+
+        // prompt the user to enter first number
+        System.out.println(" Enter the  first number :");
+
+
 
     }
 }
